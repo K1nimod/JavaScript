355d0ea470c5ab89fb596a2dc1a3a2c1f5a62c0a
K1nimod <164170790+K1nimod@users.noreply.github.com>
2026-05-28 14:14:07 +0200

    Add files via upload

---
+++ dolgozat-befozes/befozes.mjs
@@ -0,0 +1,52 @@
+import fs from 'fs';
+
+let content = fs.readFileSync("javaScript/dolgozatok/uvegek.txt", {encoding: "Utf8"})
+
+const temp = content.trim().split(',')
+const arr = temp.map(e => +e)
+
+console.log("2.feladat:")
+const input = 50
+
+if(input < 1 || input > 200)
+{
+    console.log("Hibás adat.")
+}
+else
+{
+
+console.log(`Mari néni lekvárja (dl): ${input}`)
+
+console.log("3.feladat:")
+let max = arr[0]
+let index = 0
+
+for(let i = 0; i < arr.length; i++)
+{
+    if(max < arr[i])
+    {
+        max = arr[i];
+        index = (i + 1) 
+        
+    }
+    
+}
+console.log(`A legnagyobb üveg: ${max} dl és ${index}. a sorban.`)
+
+console.log("4.feladat")
+
+let ossz = 0
+for(let i = 0;i < arr.length;i++)
+{
+    ossz += arr[i] 
+}
+
+if(ossz > input)
+{
+    console.log("Elegendő üveg volt.")
+}
+else
+{
+    console.log("Maradt lekvár.")
+}
+}
